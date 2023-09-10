bf333d237377052b6e50d01d93637191159f23f8
Andres Verdenelli <andresverdenelli@icloud.com>
2023-09-10 13:24:59 +0200

    01 exercise done

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', playSound)
+
+function playSound(e) {
+	const audio = this.document.querySelector(`audio[data-key=${e.code}]`)
+	const key = this.document.querySelector(`.key[data-key=${e.code}]`)
+	if (!audio) return
+	audio.currentTime = 0
+	audio.play()
+	key.classList.add('playing')
+}
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return
+	this.classList.remove('playing')
+}
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
